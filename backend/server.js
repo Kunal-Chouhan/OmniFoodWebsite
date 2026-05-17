5c247a9d047c7a046055102da2599f001a77a717
Kunal Chouhan <kunalchouhan037@gmail.com>
2026-05-17 15:37:28 +0530

    food website

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ require('dotenv').config(); // Load variables from .env file
 
 // --- SETUP ---
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // --- MIDDLEWARE ---
 app.use(cors()); // Allow requests from other origins (your frontend)
